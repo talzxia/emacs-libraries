8d5e9ad3bb203a5c9b7581c67262053711fb0ef1
Štěpán Němec <stepnem@gmail.com>
2011-10-23 12:59:18 +0200

    goto-chg: Minor copy-edits, whitespace fixes.

--- goto-chg.el
+++ goto-chg.el
@@ -23,12 +23,12 @@
 ;;
 ;;; Commentary:
 ;;
-;; Goto Last Change
+;; Go to Last Change
 ;;
-;; Goto the point of the most recent edit in the buffer.
-;; When repeated, goto the second most recent edit, etc.
+;; Go to the point of the most recent edit in the buffer.
+;; When repeated, go to the second most recent edit, etc.
 ;; Negative argument, C-u -, for reverse direction.
-;; Works by looking into buffer-undo-list to find points of edit.
+;; Works by looking into `buffer-undo-list' to find points of edit.
 ;;
 ;; You would probably like to bind this command to a key.
 ;; For example in your ~/.emacs:
@@ -61,7 +61,7 @@
 ;;todo: Rename "goto-chg.el" -> "gotochange.el" or "goto-chgs" ?
 ;;todo: Rename function goto-last-change -> goto-last-edit ?
 ;;todo: Rename adjective "-last-" -> "-latest-" or "-most-recent-" ?
-;;todo: There are some, maybe useful, funcs  for region undo 
+;;todo: There are some, maybe useful, funcs  for region undo
 ;;       in simple.el in emacs 20. Take a look.
 ;;todo: Add functionality to visit changed point in text order, not only in
 ;;        chronological order. (Naa, highlight-changes-mode does that).
@@ -81,9 +81,12 @@
 
 ;;; Code:
 
-(defvar glc-default-span 8 "*goto-last-change don't visit the same point twice. glc-default-span tells how far around a visited point not to visit again.")
-(defvar glc-current-span 8 "Internal for goto-last-change.\nA copy of glc-default-span or the ARG passed to goto-last-change.")
-(defvar glc-probe-depth 0 "Internal for goto-last-change.\nIt is non-zero between successive goto-last-change.")
+(defvar glc-default-span 8
+  "*Don't visit changes less than this many characters from last visit.")
+(defvar glc-current-span 8 "Internal variable used in `goto-last-change'.")
+(defvar glc-probe-depth 0
+  "Internal variable used in `goto-last-change'.
+Non-zero between successive `goto-last-change' invocations.")
 
 ;;todo: Find begin and end of line, then use it somewhere
 
@@ -97,7 +100,7 @@ Optional third argument is the replacement, which defaults to \"...\"."
     (let* ((lipsis (or ellipsis "..."))
 	   (i (/ (- maxlen (length lipsis)) 2)))
       (concat (substring str 0 i)
-	      lipsis 
+	      lipsis
 	      (substring str (- i))))))
 
 (defun glc-adjust-pos2 (pos p1 p2 adj)
@@ -112,31 +115,38 @@ Optional third argument is the replacement, which defaults to \"...\"."
 	 nil)))
 
 (defun glc-adjust-pos (pos e)
-  "Given POS, a buffer position before the edit E, compute and return
-the \"same\" buffer position after E happened.
-Exception: return nil if POS is closer than `glc-current-span' to the edit E.
-\nInsertion edits before POS returns a larger value.
-Deletion edits before POS returns a smaller value.
-\nThe edit E is an entry from the `buffer-undo-list'. See for details."
-  (cond ((atom e)			; nil==cmd boundary, or, num==changed pos
+  "Return the \"same\" buffer position as POS after edit E.
+Exception: returns nil if POS is closer than `glc-current-span' to the edit E.
+
+Insertion edits before POS return a larger value.
+Deletion edits before POS return a smaller value.
+
+The edit E is an entry from the `buffer-undo-list', which see for details."
+  (cond ((atom e)                    ; nil==cmd boundary, or, num==changed pos
 	 pos)
 	((numberp (car e))		; (beg . end)==insertion
 	 (glc-adjust-pos2 pos (car e) (car e) (- (cdr e) (car e))))
-;; 	 (cond ((< pos (- (car e) glc-current-span)) pos)
-;; 	       ((> pos (+ (car e) glc-current-span)) (+ pos (- (cdr e) (car e))))
-;; 	       (t nil)))
-	((stringp (car e))		; (string . pos)==deletion
-	 (glc-adjust-pos2 pos (abs (cdr e)) (+ (abs (cdr e)) (length (car e))) (- (length (car e)))))
-;; 	 (cond ((< pos (- (abs (cdr e)) glc-current-span)) pos)
-;; 	       ((> pos (+ (abs (cdr e)) (length (car e)) glc-current-span)) (- pos (length (car e))))
-;; 	       (t nil)))
-	((null (car e))			; (nil prop val beg . end)==prop change
-	 (glc-adjust-pos2 pos (nth 3 e) (nthcdr 4 e) 0))
-;; 	 (cond ((< pos (- (nth 3 e) glc-current-span)) pos)
-;; 	       ((> pos (+ (nthcdr 4 e) glc-current-span)) pos)
-;; 	       (t nil)))
-	(t				; (marker . dist)==marker moved
-	 pos)))
+        ;; (cond ((< pos (- (car e) glc-current-span)) pos)
+        ;;       ((> pos (+ (car e) glc-current-span))
+        ;;        (+ pos (- (cdr e) (car e))))
+        ;;       (t nil)))
+        ((stringp (car e))              ; (string . pos)==deletion
+         (glc-adjust-pos2 pos
+                          (abs (cdr e))
+                          (+ (abs (cdr e)) (length (car e)))
+                          (- (length (car e)))))
+        ;; (cond ((< pos (- (abs (cdr e)) glc-current-span)) pos)
+        ;;       ((> pos (+ (abs (cdr e)) (length (car e))
+        ;;                  glc-current-span))
+        ;;        (- pos (length (car e))))
+        ;;       (t nil)))
+        ((null (car e))                ; (nil prop val beg . end)==prop change
+         (glc-adjust-pos2 pos (nth 3 e) (nthcdr 4 e) 0))
+        ;; 	 (cond ((< pos (- (nth 3 e) glc-current-span)) pos)
+        ;; 	       ((> pos (+ (nthcdr 4 e) glc-current-span)) pos)
+        ;; 	       (t nil)))
+        (t                              ; (marker . dist)==marker moved
+         pos)))
 
 ;; If recursive in stead of iterative (while), it tends to fill the call stack.
 ;; (Isn't it tail optimized?)
@@ -145,7 +155,7 @@ Deletion edits before POS returns a smaller value.
 Pick the point of the first edit entry and update that point with
 the second, third, etc, edit entries. Return the final updated point,
 or nil if the point was closer than `glc-current-span' to some edit in R.
-\nR is basically a reversed slice from the buffer-undo-list."
+R is basically a reversed slice from `buffer-undo-list'."
   (if r
       ;; Get pos
       (let ((pos (glc-get-pos (car r))))
@@ -159,9 +169,9 @@ or nil if the point was closer than `glc-current-span' to some edit in R.
     nil))
 
 (defun glc-get-pos (e)
-  "If E represents an edit, return a position value in E, the position
-where the edit took place. Return nil if E represents no real change.
-\nE is a entry in the buffer-undo-list."
+  "If E represents an edit, return a the position where the edit took place.
+Return nil if E represents no real change.
+E is an entry in `buffer-undo-list'."
   (cond ((numberp e) e)			; num==changed position
 	((atom e) nil)			; nil==command boundary
 	((numberp (car e)) (cdr e))	; (beg . end)==insertion
@@ -173,14 +183,15 @@ where the edit took place. Return nil if E represents no real change.
 (defun glc-get-descript (e &optional n)
   "If E represents an edit, return a short string describing E.
 Return nil if E represents no real change.
-\nE is a entry in the buffer-undo-list."
+E is an entry in `buffer-undo-list'."
   (let ((nn (or (format "T-%d: " n) "")))
     (cond ((numberp e) "New position")	; num==changed position
 	  ((atom e) nil)		; nil==command boundary
 	  ((numberp (car e))		; (beg . end)==insertion
 	   (if (and n (< n 2))
-	       (format "%sInserted %d chars \"%s\"" nn (- (cdr e) (car e)) 
-		       (glc-center-ellipsis (buffer-substring (car e) (cdr e)) 60))
+	       (format "%sInserted %d chars \"%s\"" nn (- (cdr e) (car e))
+		       (glc-center-ellipsis (buffer-substring (car e) (cdr e))
+                                            60))
 	     ;; else
 	     ;; An older insert. The inserted text cannot easily be computed.
 	     ;; Just show the char count.
@@ -194,12 +205,12 @@ Return nil if E represents no real change.
 
 (defun glc-is-positionable (e)
   "Return non-nil if E is an insertion, deletion or text property change.
-\nE is a entry in the buffer-undo-list."
+E is an entry in `buffer-undo-list'."
   (and (not (numberp e)) (glc-get-pos e)))
 
 (defun glc-is-filetime (e)
-  "Return t if E indicates a buffer became \"modified\",
-that is, it was previously saved or unchanged. Nil otherwise."
+  "Return t if E indicates a buffer became \"modified\".
+That is, it was previously saved or unchanged. Nil otherwise."
   (and (listp e) (eq (car e) t)))
 
 ;;;###autoload
@@ -208,28 +219,31 @@ that is, it was previously saved or unchanged. Nil otherwise."
 Repeat the command to go to the second last edit, etc.
 A preceding \\[universal-argument] - (minus) will reverse direction for the next command in
 the sequence, to go back to a more recent edit.
-\nIt does not go to the same point twice even if there has been many edits
-there. I call the minimal distance between distinguishable edits \"span\".
-Set variable `glc-default-span' to control how close is \"the same point\".
-Default span is 8.
+
+It does not go to the same point twice even if there have been many edits
+there. Set variable `glc-default-span' to control how close \"the same point\".
+is. Default span is 8.
 The span can be changed temporarily with \\[universal-argument] right before \\[goto-last-change]:
 \\[universal-argument] <NUMBER> set current span to that number,
 \\[universal-argument] (no number) multiplies span by 4, starting with default.
 The so set span remains until it is changed again with \\[universal-argument], or the consecutive
 repetition of this command is ended by any other command.
-\nWhen span is zero (i.e. \\[universal-argument] 0) subsequent \\[goto-last-change] visits each and
+
+When span is zero (i.e. \\[universal-argument] 0) subsequent \\[goto-last-change] visits each and
 every point of edit and a message shows what change was made there.
 In this case it may go to the same point twice.
-\nThis command uses undo information. If undo is disabled, so is this command.
+
+This command uses undo information. If undo is disabled, so is this command.
 At times, when undo information becomes too large, the oldest information is
-discarded. See variable `undo-limit'."
+discarded. See the variable `undo-limit'."
   (interactive "P")
   (cond ((not (eq this-command last-command))
 	 ;; Start a glc sequence
 	 ;; Don't go to current point if last command was an obvious edit
 	 ;; (yank or self-insert, but not kill-region). Makes it easier to
-	 ;; jump back and forth when copying seleced lines.
-	 (setq glc-probe-depth (if (memq last-command '(yank self-insert-command)) 1 0)
+	 ;; jump back and forth when copying selected lines.
+	 (setq glc-probe-depth (if (memq last-command
+                                         '(yank self-insert-command)) 1 0)
 	       glc-direction 1
 	       glc-current-span glc-default-span)
 	 (if (< (prefix-numeric-value arg) 0)
@@ -242,7 +256,8 @@ discarded. See variable `undo-limit'."
 	 (setq glc-current-span (abs arg)))
 	((consp arg)			; C-u's multiply previous span by 4
 	 (setq glc-current-span (* (abs (car arg)) glc-default-span))
-	 (message "Current span is %d chars" glc-current-span))) ;todo: keep message with "waiting" and "is saved"
+         ;; todo: keep message with "waiting" and "is saved"
+	 (message "Current span is %d chars" glc-current-span)))
   (cond ((< (prefix-numeric-value arg) 0)
 	 (setq glc-direction -1))
 	(t
@@ -250,10 +265,10 @@ discarded. See variable `undo-limit'."
   (let (rev				; Reversed (and filtered) undo list
 	pos				; The pos we look for, nil until found
 	(n 0)				; Steps in undo list (length of 'rev')
-	(l buffer-undo-list) 
+	(l buffer-undo-list)
 	(passed-save-entry (not (buffer-modified-p)))
 	(new-probe-depth glc-probe-depth))
-    ;; Walk back and forth in the buffer-undo-list, each time one step deeper,
+    ;; Walk back and forth in `buffer-undo-list', each time one step deeper,
     ;; until we can walk back the whole list with a 'pos' that is not coming
     ;; too close to another edit.
     (while (null pos)
